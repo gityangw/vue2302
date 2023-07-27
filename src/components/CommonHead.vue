3b3152c43171ee0940811702fe6e38b56268c275
study <1874466727@qq.com>
2023-07-27 19:16:26 +0800

    111

---
+++ src/components/CommonHead.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>这是父组件</h2>
+    <common-nav :msg="msg"></common-nav>
+</template>
+
+<script>
+import CommonNav from './CommonNav.vue';
+export default {
+    data() {
+        return {
+            msg: '这是子组件的数据'
+        }
+    },
+    components: {
+        CommonNav
+    }
+}
+</script>
---
+++ src/components/CommonNav.vue
@@ -0,0 +1,17 @@
+<template>
+    这是子组件
+    {{ msg }}
+</template>
+
+<script>
+    export default {
+        props: ['msg'],
+        mounted(){
+            console.log(this);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
