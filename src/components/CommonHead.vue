2ef07ea5b0a419dc23b73370209ac89b33a9d32f
study <1874466727@qq.com>
2023-07-31 19:14:25 +0800

    2

--- src/components/CommonHead.vue
+++ src/components/CommonHead.vue
@@ -22,7 +22,7 @@ export default {
 <style scoped>
 /* 定义入场初始状态 */
 .ani1-enter-from {
-  transform: translateX(-500px) rotate(-360deg) scale(0.1);
+  transform: translateX(-500px) scale(0.1);
   opacity: 0;
 }
 /* 过渡类中使用 transition */
@@ -32,7 +32,7 @@ export default {
 }
 /* 定义出场最终状态样式 */
 .ani1-leave-to {
-  transform: translateX(500px) rotate(360deg) scale(0.1);
+  transform: translateX(500px) scale(0.1);
   opacity: 0;
 }
 .box {
